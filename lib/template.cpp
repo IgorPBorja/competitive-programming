7711bd028ef05129078eabba7f29c10dc18e369b
IgorPBorja <igorpradoborja@gmail.com>
2025-07-12 23:46:04 -0300

    feat: add HLD implementation to lib

--- lib/template.cpp
+++ lib/template.cpp
@@ -1,4 +1,6 @@
 #define TESTCASES
+#define debug cerr
+
 #include <bits/stdc++.h>
 using namespace std;
 #define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
@@ -8,20 +10,28 @@ using namespace std;
 #define u64 uint64_t
 #define i128 __int128
 #define all(x) begin(x),end(x)
-#define printmsg(msg, first, last) cout << msg; for(auto it = first; it != last; it++){cout << *it << " ";} cout << endl;
-#define printgraph(msg, G) cout << msg << endl; \
+#define printmsg(msg, first, last) debug << msg; for(auto it = first; it != last; it++){debug << *it << " ";} debug << endl;
+#define printgraph(msg, G) debug << msg << endl; \
 	for (u64 u = 0; u < G.size(); u++) { \
-		cout << "G[" << u << "]="; \
+		debug << "G[" << u << "]="; \
 		for (u64 v: G[u]) { \
-			cout << v << " "; \
+			debug << v << " "; \
 		} \
-		cout << endl; \
+		debug << endl; \
 	}
  
 template <typename T, typename U>
 ostream& operator<< (ostream& out, pair<T, U> x)
 {
 	out << x.first << " " << x.second;
+	return out;
+}
+template <typename T>
+ostream& operator<< (ostream& out, vector<T> v)
+{
+	for (const auto& x: v){
+		out << x << " ";
+	}
 	return out;
 }
 
---
+++ lib/trees/hld.cpp
@@ -0,0 +1,107 @@
+// HLD implementation
+// Tested on CSES Path Queries (https://cses.fi/problemset/task/1138)
+// Query: sum of vertices from root to v
+// Update: set value of v to x
+
+#include <bits/stdc++.h>
+using namespace std;
+#define i64 int64_t
+
+// uses some modifications: now the heavy edge is the edge with the largest subtree
+// instead of the restriction s(c) >= s(v) / 2
+// Still, going through light (i.e not heavy) edge reduces size by half
+// Heavy path goes up to just the vertex before light edge
+using tree = vector<vector<i64>>;
+struct HLD {
+    vector<i64> heavy;   // heavy[u] = v <==> uv is heavy edge
+    vector<i64> sz;
+    vector<i64> head;  // head[u] = start of heavy path that goes through u
+    vector<i64> pos;   // depth of vertex in path. Head has depth 0
+    vector<i64> path_id;  // id of heavy path of vertex
+    vector<i64> path_head;  // head per path
+    vector<i64> parent;
+    vector<SegTree> segs;  // NOTE: SegTree is the segment tree implementation
+    i64 uuid = 0;
+
+    HLD(const tree& adj, const vector<i64>& a, i64 root = 0){
+        const i64 n = adj.size();
+        heavy.resize(n);
+        sz.resize(n);
+        head.resize(n);
+        pos.resize(n);
+        path_id.resize(n);
+        parent.resize(n);
+
+        subtree(root, adj);
+        dfs(root, adj);
+
+        // build segment trees
+        for (i64 h: path_head){
+            vector<i64> path;
+            i64 cur = h;
+            while (cur != -1 && head[cur] == h){
+                path.emplace_back(a[cur]);  // add value
+                cur = heavy[cur];
+            }
+            segs.emplace_back(SegTree(path));
+        }
+    }
+
+    void subtree(i64 u, const tree& adj, i64 p = -1){
+        sz[u] = 1;
+        for (i64 v: adj[u]){
+            if (v == p) continue;
+            subtree(v, adj, u);
+            sz[u] += sz[v];
+        }
+    }
+
+    void dfs(i64 u, const tree& adj, i64 p = -1, i64 h = 0, i64 d = 0){
+        i64 cur_sz = 0;
+        pos[u] = d;
+        head[u] = h;
+        parent[u] = p;
+        if (h == u){
+            // starting new heavy path: on each path use new uuid
+            path_id[u] = uuid++;
+            path_head.emplace_back(u);
+        } else {
+            path_id[u] = path_id[head[u]];
+        }
+
+        heavy[u] = -1;
+        for (i64 v: adj[u]){
+            if (v == p) continue;
+            if (sz[v] > cur_sz){
+                cur_sz = sz[v];
+                heavy[u] = v;
+            }
+        }
+        for (i64 v: adj[u]){
+            if (v == p) continue;
+            if (v == heavy[u]){
+                dfs(v, adj, u, h, d + 1);
+            } else {
+                dfs(v, adj, u, v, 0);
+            }
+        }
+    }
+
+    // query from u to p^k(u) inclusive
+    // NOTE: SUM QUERY HERE, other variations are possible
+    i64 query_up(i64 u, i64 k){
+        i64 s = 0;
+        while (u != -1 && k >= pos[u]){
+            s += segs[path_id[u]].query(0, pos[u]);
+            u = parent[head[u]];
+        }
+        if (u != -1){  // k < pos[u]
+            s += segs[path_id[u]].query(pos[u] - k, pos[u]);
+        }
+        return s;
+    }
+
+    void update(i64 u, i64 x){  // set value to x
+        segs[path_id[u]].update(pos[u], x);
+    }
+};
